c898226bb435cf258e057138a10189aa2e81da45
jixiuf <jixiuf@gmail.com>
2011-12-04 22:38:35 +0800

    %% {next_state, NextStateName, NewStateData} 当返回next_state 的状态时 必须用    gen_fsm:reply(From,my_reply), 先给一个回应 v3

--- fsm3.erl
+++ fsm3.erl
@@ -6,6 +6,18 @@
 %% 对于StateName/3 的方法 ，由gen_fsm:sync_send_event/3来触发 此为同步发送
 %%似乎要用 {reply,my_reply,state1,"state data of state1."}作返回值
 %% 我用{next_state,state1,"state data of state1."}作返回值，报timeout
+%% {next_state, NextStateName, NewStateData} 当返回next_state 的状态时 必须用    gen_fsm:reply(From,my_reply), 先给一个回应
+
+%%statename/3 可以返回
+%% {reply, Reply, NextStateName, NewStateData}
+%% {reply, Reply, NextStateName, NewStateData, Timeout}
+%% {reply, Reply, NextStateName, NewStateData, hibernate}
+%% {next_state, NextStateName, NewStateData} 当返回next_state 的状态时 必须用    gen_fsm:reply(From,my_reply), 先给一个回应
+%% {next_state, NextStateName, NewStateData, Timeout}
+%% {next_state, NextStateName, NewStateData, hibernate}
+%% {stop, Reason, Reply, NewStateData}
+%% {stop, Reason, NewStateData}
+
 %%
 start()->
     {ok,Pid}=    gen_fsm:start_link(?MODULE,init_data,[]),
@@ -24,8 +36,9 @@ state1(event1,StateData) ->
 
 state1(event1,From,StateData) ->
     timer:sleep(1000),
+    gen_fsm:reply(From,my_reply),
     io:format("~ts~n",["server receive msg after 3 second ,server print this line"]),
-    {reply,my_reply,state1,"state data of state1."}
+    {next_state,state1,"state data of state1."}
         .
 
 send1()->
