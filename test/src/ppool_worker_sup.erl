d62291b81761acf83bb4452e566eec95f2bd51b6
jixiuf <jixiuf@gmail.com>
2011-12-07 18:04:38 +0800

    src

--- test/src/ppool_worker_sup.erl
+++ test/src/ppool_worker_sup.erl
@@ -2,17 +2,17 @@
 -behaviour(supervisor).
 -compile([export_all]).
 
-start_link(PoolName,PoolSize,MFA)->
+start_link(MFA={_,_,_})->
     io:format("ppool_worker_sup is starting ...~n",[]),
-    supervisor:start_link({local,pool_super},?MODULE,[PoolName,PoolSize,MFA])
+    supervisor:start_link({local,pool_worker_super},?MODULE,[MFA])
 .
 
-init([PoolName,PoolSize,MFA])->
+init({M,F,A})->
     io:format("ppool_worker_sup initing...~n",[]),
     MaxTime=3600,
     MaxRestart=1,
-    {ok,{{one_for_all,MaxRestart,MaxTime},
-         [{ppool_serv,                           %id
+    {ok,{{simple_one_for_one,MaxRestart,MaxTime},
+         [{pool_worker_super,                           %id
            {ppool_serv,start_link,[PoolName,PoolSize,MFA,self()]},
            permanent, 5000, worker, [ppool_serv]
           }
