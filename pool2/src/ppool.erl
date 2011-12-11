99af8ba4bc5940fbe938ff242c7d522558fb0b56
jixiuf <jixiuf@gmail.com>
2011-12-11 23:23:20 +0800

    支持.app 的start/2 stop/1 回调

--- pool2/src/ppool.erl
+++ pool2/src/ppool.erl
@@ -1,19 +1,34 @@
 -module(ppool).
--compile([export_all]).
+-export([start/2, stop/1]).
+-export([start_pool/3,stop_pool/1]).
+-export([run/2,sync_queue/2,async_queue/2]).
+
+
 %% ppool API
 
-start_link()->
-ppool_supersuper:start_link()
+
+%% start_link()->
+%% ppool_supersuper:start_link()
+%%     .
+%% replace it with
+start(normal,_Args)->
+    ppool_supersuper:start_link()
     .
+
+%% stop()->
+%%     ppool_supersuper:stop().
+%% replace with
+%% look {mod . ...} ppool.app.src for detail
+stop(_State)->
+    ok
+        .
+
+
 stop_pool(PoolName)->
 ppool_supersuper:stop_pool(PoolName).
 
 start_pool(PoolName,PoolSize,MFA4worker)->
     ppool_supersuper:start_pool(PoolName,PoolSize,MFA4worker).
-
-stop()->
-    ppool_supersuper:stop().
-
 run(Name,Args)->
     ppool_serv:run(Name,Args)
         .
