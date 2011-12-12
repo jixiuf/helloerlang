f84b356d8145c6a5f6b69b12bb0a178097ccb519
jixiuf <jixiuf@gmail.com>
2011-12-12 11:07:41 +0800

    rm warn

--- pool2/test/worker_demo.erl
+++ pool2/test/worker_demo.erl
@@ -1,6 +1,6 @@
 -module(worker_demo).
 -compile([export_all]).
--behaviour(gen_server).
+-export([handle_info/2,init/1,terminate/2]).
 
 start_link(Delay,Msg)->
     gen_server:start_link(?MODULE,[Delay,Msg],[] )
@@ -10,17 +10,17 @@ init([Delay,Msg])->
     io:format("a worker is initing...~n",[]),
     io:format("after ~p seconds ,this worker will die ,the message will be: ~p.~n",[Delay,Msg]),
     Pid = spawn(?MODULE,action,[Delay,Msg]),
-    Ref = erlang:monitor(process,Pid),
-        {ok,{Delay,Msg}}
+    erlang:monitor(process,Pid),
+    {ok,{Delay,Msg}}
         .
-handle_info({'DOWN',Ref,process,Pid,Reason} ,State)->
+handle_info({'DOWN',_Ref,process,_Pid,_Reason} ,_State)->
     %%当action/2 对应进程down ,会收到此信息，然后当前gen_server/退出，退出时ppool_serv 会收到信息，
     %%而后，从pool中清除此条process
     exit(normal)
 
         .
 
-terminate(M,S)->
+terminate(_M,_S)->
     ok
     .
 
