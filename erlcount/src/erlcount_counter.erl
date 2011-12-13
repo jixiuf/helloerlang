67aeafac54d51e46fd4e29d71d6f1fca30c65fea
jixiuf <jixiuf@gmail.com>
2011-12-13 12:50:12 +0800

    a

---
+++ erlcount/src/erlcount_counter.erl
@@ -0,0 +1,46 @@
+-module(erlcount_counter).
+-record(state,{dispatchingpid,file,ref,regexp}).
+
+
+start_link(DispatchingPid,File,Ref,Re)->
+    io:format("erlcount_counter starting... for ~p in ~p~n",[Re,File]),
+    gen_server:start_link(?MODULE,[DispatchingPid,File,Ref],[])
+    .
+
+init(DispatchingPid,File,Ref,Re)->
+    io:format("erlcount_counter initing... for ~p in ~p~n",[Re,File]),
+    self()!start,
+    {ok,#state{dispatchingpid=DispatchingPid,file=File,ref=Ref,regexp=Re}}
+        .
+
+handle_call(_Request,_From,State)->
+    {noreply, State}
+        .
+
+handle_cast(Msg,State)->
+    {noreply, State} .
+
+handle_info(start,State=#state{file=File,regexp=Re,dispatchingpid=DispatchingPid,ref=Ref}) ->
+
+    ;
+handle_info(Info,State)->
+    {noreply, State}.
+
+terminate(Reason,State=#state{regexp=Re,file=File})->
+    io:format("erlcount_counter handleing regexp:~p in file:~p done!~ ~n",[Re,File]),
+    ok
+        .
+
+code_change(Previous_Version,State,Extra)->
+    {ok,State}
+        .
+
+%%regexp_count("a","abab")
+regexp_count(Regexp,Str)->
+    case re:run(Str,Regexp,[global]) of
+        nomatch->
+            0;
+        {match,List} ->
+            length(List)
+    end
+        .
