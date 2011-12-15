ac99a614ef370173aa0b45ce91d9b3a010f0435a
jixiuf <jixiuf@gmail.com>
2011-12-15 12:59:37 +0800

    erlcount_log:info("erlcount is starting...",[]),

--- erlcount_release/erlcount/src/erlcount_sup.erl
+++ erlcount_release/erlcount/src/erlcount_sup.erl
@@ -3,11 +3,12 @@
 -export([start_link/0,init/1]).
 
 start_link()->
-    io:format("erlcount_sup is staring... ~n",[]),
+    erlcount_log:info("erlcount is starting...",[]),
+    erlcount_log:debug("erlcount_sup is staring... ~n",[]),
     supervisor:start_link(?MODULE,[])
         .
 init([])->
-    io:format("erlcount_sup is initing...~n",[]),
+    erlcount_log:debug("erlcount_sup is initing...~n",[]),
     MaxRestart=3,
     MaxTime=5000,
     {ok,{{one_for_one,MaxRestart,MaxTime},
