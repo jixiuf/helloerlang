1b30812ac4700147ee25203bdebf02843753756f
jixiuf <jixiuf@gmail.com>
2011-12-20 19:58:43 +0800

    comment

--- simple_one_for_one/main.erl
+++ simple_one_for_one/main.erl
@@ -4,6 +4,8 @@
 start_link()->
     io:format("starting app...~n",[]),
     {ok,Supid}=   sup:start_link() ,
-    supervisor:start_child(Supid,[" world"]),
-    supervisor:start_child(Supid,[" world2"])
+    {ok,WorkerPid}=supervisor:start_child(Supid,[" world"]),
+    erlang:monitor(process,WorkerPid),          %可以监测到worker pid  ,flush()  后Shell got {'DOWN',#Ref<0.0.0.79>,process,<0.49.0>,normal}
+
+    supervisor:start_child(Supid,[" world2"])   %可以启动多个worker ,各个worker 完成自已的任务后自行退出.
         .
