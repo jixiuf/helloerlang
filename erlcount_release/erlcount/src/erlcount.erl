11e3cda9ef2d4bb2ca86c00ca62f6b23bf6479a5
jixiuf <jixiuf@gmail.com>
2011-12-20 20:51:20 +0800

    time

--- erlcount_release/erlcount/src/erlcount.erl
+++ erlcount_release/erlcount/src/erlcount.erl
@@ -1,11 +1,17 @@
 -module(erlcount).
--export([start/2,stop/1]).
+-export([start/2,stop/1,get_timestamp/0]).
+
+get_timestamp() ->
+    {Mega,Sec,Micro} = erlang:now(),
+    (Mega*1000000+Sec)*1000000+Micro.
 
 start(normal,_Args)->
+    io:format("start time:~p~n",[get_timestamp()]),
     erlcount_log:info("erlcount is starting...~n",[]),
     erlcount_sup:start_link()
     .
 
 stop(_State)->
+    io:format("end time:~p~n",[get_timestamp()]),
     ok
-    .
+        .
