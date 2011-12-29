e492f37c525abac18ae48ccd764682ec5a2ca999
jixiuf <jixiuf@gmail.com>
2011-12-29 11:13:11 +0800

    %% io:read  test，注意，不是file:read

--- io/iotest.erl
+++ io/iotest.erl
@@ -1,6 +1,6 @@
 %%    -*- coding:utf-8 -*-
 -module(iotest).
--export([write/0,read_line/0,read_all_file/0,write_file/0,read_file_line_by_line/0,consult/0]).
+-export([write/0,read_line/0,read_all_file/0,write_file/0,read_file_line_by_line/0,consult/0,read/0]).
 
 read_line()->
     {ok,Fh}= file:open("iotest.erl",read),
@@ -47,10 +47,22 @@ write()->
     .
 
 consult()->
-     %% {ok,Term} = file:consult("test.dat")
+    %% file:consult 用来处理 erlang 格式的 term 数据file:consult会假定file指定
+    %% 的文件中包含一串erlang的数据项,如果可以读取文件中的所有数据项,那么返回
+    %% {ok,[Term]},否则返回{error,Reason}
+    %% {ok,Term} = file:consult("test.dat")
+    %% test.dat 中只有一个{key1,value}. 的term ,当然也可以用多个{{},[]}之类
      {ok,[{key1,Val}]} = file:consult("test.dat"),
         io:format("value of key1 is ~p~n",[Val])
         %% ,
     %% io:format("~p~n",[Term])
 
     .
+
+read()->
+    %% io:read  test，注意，不是file:read
+    %%io:read 与file:consult 的不同是，前者只读取一个term ,后者全文读出
+    {ok,FileH}=file:open("test.dat",read),
+    {ok,Term1}=io:read(FileH,''),               %
+    io:format("~p~n",[Term1])
+        .
