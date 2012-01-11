9d9340cdcf45deb1cc316e02af6117ceeaeae5b8
jixiuf <jixiuf@gmail.com>
2012-01-11 11:48:27 +0800

    work now

--- chat/src/client.erl
+++ chat/src/client.erl
@@ -1,6 +1,13 @@
 -module(client).
+-export([close/1,echo/2,connect/2]).
+
 
 connect(Host,Port) ->
-    {ok, Sock} = gen_tcp:connect(Host, Port, [binary]),
-    ok = gen_tcp:send(Sock, <<4:32,"echo","hello">>),
-    ok = gen_tcp:close(Sock).
+    gen_tcp:connect(Host, Port, [binary])
+        .
+
+echo(Socket,Msg) when is_list(Msg)->            %Msg is string
+    ok = gen_tcp:send(Socket,iolist_to_binary([<<4:32,"echo">>,Msg]) )
+        .
+close(Socket)->
+    ok = gen_tcp:close(Socket).
