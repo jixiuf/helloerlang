5ee5b0a1355fd1a1487f8eb56df48d54f9a332f8
jixiuf <jixiuf@gmail.com>
2012-01-18 13:12:22 +0800

    client

--- chat/src/client.erl
+++ chat/src/client.erl
@@ -3,11 +3,11 @@
 
 
 connect(Host,Port) ->
-    case gen_tcp:connect(Host, Port, [binary,{packet,4}]) of
+    case gen_tcp:connect(Host, Port, [binary,{active,false},{packet,4}]) of
         {ok,ServerSocket}->
             Pid= spawn(?MODULE,do_recv,[ServerSocket]),
-            %% gen_tcp:controlling_process(ServerSocket, Pid),
-            %% gen_tcp:recv(ServerSocket,0)
+            register(?MODULE,Pid),              %pid of sending msg to server
+            gen_tcp:controlling_process(ServerSocket, Pid),
             {ok,ServerSocket}
             %% timer:sleep(infinity)
                 ;
@@ -16,34 +16,46 @@ connect(Host,Port) ->
     end
         .
 do_recv(ServerSocket)->
-    io:format("helllllll~n",[]),
-    case   gen_tcp:recv(ServerSocket,0) of
-        {ok,Bin}->
-            case util:read_int32(Bin) of
-                {Int_Of_Bit,CommandBody} when is_integer(Int_Of_Bit) and is_binary(CommandBody)->
-                    case CommandBody of
-                        <<Command:Int_Of_Bit,Tail/binary>> ->
-                            handle_command(<<Command:Int_Of_Bit>>,Tail,ServerSocket),
-                            do_recv(ServerSocket);
-                        _Other ->
-                            do_recv(ServerSocket)
-                    end
-
-                        ;
-                _Other ->
-                    do_recv(ServerSocket)
-            end ;
-        {error,Reason} ->
+    chat_log:debug("client do recving...~n",[]),
+    inet:setopts(ServerSocket, [{active, once}]),
+    receive
+        {send,Bin,Socket}->
+            chat_log:debug("client sending data to server ...~n",[]) ,
+            gen_tcp:send(Socket,Bin),          %send Bin to ServerSocket
+            do_recv(ServerSocket);
+        {tcp, ServerSocket, Bin}->
+            handle_command(Bin,ServerSocket),
+            do_recv(ServerSocket);
+        {tcp_closed, ServerSocket} ->
+            chat_log:debug("server down!!!!!~n",[]),
+            do_recv(ServerSocket) ;
+        {tcp_error, ServerSocket, Reason} ->
+            chat_log:debug("error ~p~n",[Reason]) ,
             {error,Reason}
-        end
-    .
-handle_command(<<"echo">>,Bin,_ServerSocket)->
-    io:format("server said:~p~n",[binary_to_list(Bin)])
+    end
+    %% case   gen_tcp:recv(ServerSocket,0) of
+    %%     {send,Bin,Socket}->
+    %%         io:format("client sending data to server ...~n",[]) ,
+    %%         %% gen_tcp:send(Socket,Bin),          %send Bin to ServerSocket
+    %%         do_recv(ServerSocket);
+    %%     {ok,Bin}->
+    %%         handle_command(Bin,ServerSocket),
+    %%         do_recv(ServerSocket);
+    %%     {error,Reason} ->
+    %%         io:format("error ~p~n",[Reason]) ,
+    %%         {error,Reason};
+    %%     OtherMsg ->
+    %%         io:format("OtherMsg:~p~n",[OtherMsg]),
+    %%         do_recv(ServerSocket)
+    %% end
+        .
+handle_command(<<1:32,EchoMsg/binary>>,_ServerSocket)->
+    io:format("server said:~p~n",[binary_to_list(EchoMsg)])
     .
 
 echo(Socket,Msg) when is_list(Msg)->            %Msg is string
-    ok = gen_tcp:send(Socket,<<1:32,"hello">> )
-    %% ok = gen_tcp:send(Socket,iolist_to_binary([util:encode_command("echo"),Msg]) )
-        .
+    whereis(?MODULE) ! {send ,util:binary_concat(<<1:32>>,<<"hello">>),Socket},
+    ok .
+
 close(Socket)->
     ok = gen_tcp:close(Socket).
