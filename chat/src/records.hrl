ccef962bda709e6d64cfecb9902e1f71c20c8cf2
jixiuf <jixiuf@gmail.com>
2012-01-20 23:05:31 +0800

    update rec

---
+++ chat/src/records.hrl
@@ -0,0 +1,6 @@
+-record(room,{room_name,client_socket_id}).
+-record(user,{id,name,nickname,password}).
+
+%记录当前有哪些活动用户，最后一次活动时间,如果发现用户长时间 不活动，
+%% 可以从此表中删除此记录，及socket 信息清理等
+-record(activated_user,{userid,client_socket_id,update_time }).
