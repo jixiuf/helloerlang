d69c3cc07a7464a71f0914e3e37b61b446ee49d8
jixiuf <jixiuf@gmail.com>
2012-01-21 22:00:26 +0800

    -record(user,{name,nickname,password}).

--- chat/src/records.hrl
+++ chat/src/records.hrl
@@ -1,5 +1,6 @@
 -record(room,{room_name,client_socket_id}).
--record(user,{id,name,nickname,password}).
+%% -record(user,{id,name,nickname,password}).
+-record(user,{name,nickname,password}).
 
 %记录当前有哪些活动用户，最后一次活动时间,如果发现用户长时间 不活动，
 %% 可以从此表中删除此记录，及socket 信息清理等
