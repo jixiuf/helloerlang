98f4cc72c8a735a43f7fb15c929ca11ddb939f81
jixiuf <jixiuf@gmail.com>
2012-02-11 14:50:48 +0800

    timestamp_to_datetime(TimeStamp)->

--- util.erl
+++ util.erl
@@ -1,5 +1,5 @@
 -module(util).
--export([time_to_string/1]).
+-export([datetime_to_timestamp/1,timestamp_to_datetime/1,time_to_string/1]).
 
 %% Time (as return by calendar:local_time() to string conversion.
 %% "2012-01-06 09:55:12"
@@ -8,3 +8,15 @@ time_to_string( {{Y,Mo,D},{H,Mi,S}} ) ->
                             [Y,Mo,D,H,Mi,S] ),
 
          lists:flatten(String).
+
+timestamp_to_datetime(TimeStamp)->
+    Second1 = calendar:datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}}),
+    DateTime = TimeStamp +Second1,
+    calendar:datetime_to_gregorian_seconds(DateTime)
+    .
+
+datetime_to_timestamp({Date,Time})->
+    Second1 = calendar:datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}}),
+    Second2=calendar:datetime_to_gregorian_seconds({Date,Time}),
+    Second2-Second1
+        .
