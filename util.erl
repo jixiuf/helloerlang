515aa7f6d3984af0c68f85074ad64be6c3676994
jixiuf <jixiuf@gmail.com>
2012-02-11 15:18:15 +0800

    string_2_date(DateStr)->     list_to_tuple( lists:map(fun erlang:list_to_integer/1, string:tokens(DateStr,"-"))).

--- util.erl
+++ util.erl
@@ -1,5 +1,5 @@
 -module(util).
--export([datetime_to_timestamp/1,timestamp_to_datetime/1,time_to_string/1]).
+-export([string_2_date/1,datetime_to_timestamp/1,timestamp_to_datetime/1,time_to_string/1]).
 
 %% Time (as return by calendar:local_time() to string conversion.
 %% "2012-01-06 09:55:12"
@@ -19,4 +19,7 @@ time_to_string( {{Y,Mo,D},{H,Mi,S}} ) ->
 %%     Second1 = calendar:datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}}),
 %%     Second2=calendar:datetime_to_gregorian_seconds({Date,Time}),
 %%     Second2-Second1
-%%         .
+%%
+%%string_2_date("2011-1-1")
+string_2_date(DateStr)->
+    list_to_tuple( lists:map(fun erlang:list_to_integer/1, string:tokens(DateStr,"-"))).
