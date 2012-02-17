c5ae0195790e65938f2b8dfa8c6ebe7f1fbd251c
jixiuf <jixiuf@gmail.com>
2012-02-17 16:12:11 +0800

    simple one for one

--- sup/simple_one_for_one/worker.erl
+++ sup/simple_one_for_one/worker.erl
@@ -1,37 +1,8 @@
 -module(worker).
--export([start_link/1,code_change/3,handle_call/3,handle_cast/2,handle_info/2,init/1,terminate/2]).
--export([stop/1]).
+-export([start_link/1]).
 
+%% 似乎worker 的返回值必须是{ok,Pid}
 start_link(S)->
-    gen_server:start_link(?MODULE,S,[])
-.
-init(S)->
     io:format("~p~n",[S]),
-    self()! ("hello"++S),
-    {ok,S}
+    {ok, spawn(fun()-> timer:sleep(1000) end)}
         .
-
-handle_call(stop,_From,State)->
-    {stop,State};
-handle_call(_Request,_From,State)->
-    {noreply, State}
-        .
-
-handle_cast(_Request,State)->
-    {noreply, State} .
-
-handle_info(Msg,State) ->
-    io:format("got msg:~s~n",[Msg]),
-    {stop,normal,State}.
-    %% {noreply, State}.
-
-terminate(_Reason,_State)->
-    io:format("a worker die!~n",[]),
-    ok .
-
-code_change(_Previous_Version,State,_Extra)->
-    {ok,State} .
-
-%% interface
-stop(Pid)->
-    gen_server:call(Pid,stop).
