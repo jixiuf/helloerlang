4af8bad3dc1b2ef0d101c0a223c38ae82fa6e946
jixiuf <jixiuf@gmail.com>
2012-08-24 10:40:04 +0800

    benchmark

--- benchmark/src/test_list.erl
+++ benchmark/src/test_list.erl
@@ -1,43 +1,47 @@
 -module(test_list).
 -export([test/1]).
+-define(CONFIG_75_ELEMENT,"../75.json").
+-define(CONFIG_100_ELEMENT,"../100.json").
+-define(CONFIG_200_ELEMENT,"../200.json").
+-define(CONFIG_300_ELEMENT,"../300.json").
+-define(CONFIG_500_ELEMENT,"../500.json").
+-define(CONFIG_1000_ELEMENT,"../1000.json").
 
 test(Count)->
-    io:format("count=~p,list,    time=~p ~n",[Count,test_list(Count)]),
+    io:format("75 elements in list,query count=~p,list,    time=~p ~n",
+              [Count,test_list(Count*3,?CONFIG_75_ELEMENT,[<<"1">>,<<"30">>,<<"75">>])]),
     io:format("count=~p,dict     time=~p ~n",[Count,test_dict(Count)]),
     io:format("count=~p,orddict  time=~p ~n",[Count,test_orddict(Count)])
     .
 
-test_list(Count)->
-    DataList=init_list(),
-    timer:tc(fun()-> test_list(DataList,Count)end)
+test_list(Count,JsonFile,Keys)->
+    DataList=init_list(JsonFile),
+    timer:tc(fun()-> list(DataList,Count,Keys)end)
     .
-
-
-test_dict(Count)->
-    DataList=init_dict(),
-    timer:tc(fun()-> test_dict(DataList,Count)end)
-    .
-test_orddict(Count)->
-    DataList=init_orddict(),
-    timer:tc(fun()-> test_orddict(DataList,Count)end)
-    .
-
-init_list()->
-    {ok,JsonData}=file:read_file("../test_list.json"),
+init_list(JsonFile)->
+    {ok,JsonData}=file:read_file(JsonFile),
     {struct,DataList}=mochijson2:decode(JsonData),
     DataList.
 
-test_list(_List,0)->
+list(_List,0,_Keys)->
     ok;
-test_list(List,Count)->
-    lists:keyfind(<<"1">>,1,List),
-    lists:keyfind(<<"100">>,1,List),
-    lists:keyfind(<<"200">>,1,List) ,
-    lists:keyfind(<<"300">>,1,List),
-    test_list(List,Count-1)
-        .
-init_dict()->
-    List=init_list(),
+list(List,Count,Keys)->
+    %% lists:keyfind(<<"100">>,1,List),
+    [lists:keyfind(Key,1,List)||Key<-Keys],
+    list(List,Count-1,Keys).
+
+
+test_dict(Count,JsonFile)->
+    DataList=init_dict(JsonFile),
+    timer:tc(fun()-> test_dict(DataList,Count)end)
+    .
+test_orddict(Count,JsonFile)->
+    DataList=init_orddict(JsonFile),
+    timer:tc(fun()-> test_orddict(DataList,Count)end)
+    .
+
+init_dict(JsonFile)->
+    List=init_list(JsonFile),
     dict:from_list(List).
 
 test_dict(_List,0)->
@@ -50,8 +54,8 @@ test_dict(Dict,Count)->
     test_dict(Dict,Count-1)
         .
 
-init_orddict()->
-    List=init_list(),
+init_orddict(JsonFile)->
+    List=init_list(JsonFile),
     orddict:from_list(List).
 
 test_orddict(_List,0)->
