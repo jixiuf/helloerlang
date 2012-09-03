10e0c58b99df110bafb388a51f15fbec718ff0fc
jixiuf <jixiuf@gmail.com>
2012-09-03 19:14:27 +0800

    net demo

--- net_demo/src/server_handle.erl
+++ net_demo/src/server_handle.erl
@@ -3,8 +3,20 @@
 %% 此模块中处理具体的业务逻辑
 -include_lib("base_header.hrl").
 
-handle(#c2s_protocol{header=?C2S_PROTOCOL_ECHO,body=#c2s_echo{}=EchoRecord})->
+handle(C2SProtocol)->
+    S2CProtocolList=handling(C2SProtocol),
+    filter(S2CProtocolList)
+    .
+
+handling(#c2s_protocol{header=?C2S_PROTOCOL_ECHO,body=#c2s_echo{}=EchoRecord})->
     echo_handle:handle_echo(EchoRecord);
-handle(_Protocol)->
+handling(_Protocol)->
     throw(error)
+    .
+
+%% 对某些协议进行合并，等处理
+%% 比如 代码出可能多处有加铜币的操作
+%% 对这样的协议可能需要合并成一条加铜币的协议处理
+filter(S2CProtocolList)->
+    S2CProtocolList
     .
--- net_demo/src/server_tcp.erl
+++ net_demo/src/server_tcp.erl
@@ -56,11 +56,11 @@ handle_tcp_closed(ClientSocket)->
         .
 
 handle_data(Bin,ClientSocket) ->
-    EncodeBin=
+    EncodeData=
         try
             C2SProtocol=server_decode:decode(Bin) ,
-            S2CProtocol=server_handle:handle(C2SProtocol),
-            server_encode:encode(S2CProtocol)
+            S2CProtocols=server_handle:handle(C2SProtocol),
+            [server_encode:encode(S2CProtocol)||S2CProtocol<-S2CProtocols]
         catch
             throw:Msg->
                 io:format("~p:handle_data/2 error with reason:~p~n",[?MODULE,Msg]) ,
@@ -69,4 +69,4 @@ handle_data(Bin,ClientSocket) ->
                 io:format("~p:handle_data/2 error with reason:~p~n",[?MODULE,ErrorId]) ,
                 server_encode:encode_server_error()
         end,
-    gen_tcp:send(ClientSocket,EncodeBin).
+    gen_tcp:send(ClientSocket,EncodeData).
