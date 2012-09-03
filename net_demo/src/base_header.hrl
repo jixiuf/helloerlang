32bc49f45405d27f0a8246a934e7651b494e0b6b
jixiuf <jixiuf@gmail.com>
2012-09-03 19:18:33 +0800

    net demo

--- net_demo/src/base_header.hrl
+++ net_demo/src/base_header.hrl
@@ -2,16 +2,19 @@
 
 -define(C2S_TCP_PACKET,2).
 -define(C2S_PROTOCOL_LENGTH,16).
+-define(S2C_PROTOCOL_LENGTH,16).
+
+%% c2s 协议号
 -define(C2S_PROTOCOL_ECHO,1).
 
-
--define(S2C_PROTOCOL_LENGTH,16).
+%% s2c协议号
 -define(S2C_PROTOCOL_SERVER_ERROR,0).
 -define(S2C_PROTOCOL_ECHO,1).
 
 %% errorid
 -define(S2C_ERROR_SUCCESS,0).
 -define(S2C_ERROR_FAILED,1).
+%%  可以定义其他类型的errorid,
 
 
 -record(c2s_protocol,{header,body}).
