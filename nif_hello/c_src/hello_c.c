57d2cbaf9e77d1bd8b9322f8e21fe7c09808e4ff
jixiuf <jixiuf@gmail.com>
2012-12-29 13:59:47 +0800

    [纪秀峰]:nif 受影响的文件:     nif_hello/c_src/hello_c.c

--- nif_hello/c_src/hello_c.c
+++ nif_hello/c_src/hello_c.c
@@ -1,4 +1,6 @@
 #include "erl_nif.h"
+/* 有相关博文 */
+/* http://wqtn22.iteye.com/blog/1603729 */
 
 static ERL_NIF_TERM hello_in_c(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
@@ -11,3 +13,17 @@ static ErlNifFunc nif_funcs[] =
 };
 /* hello_test 跟 hello_test.erl模块名要同 */
 ERL_NIF_INIT(hello_test,nif_funcs,NULL,NULL,NULL,NULL)
+/* ERL_NIF_INIT宏展开之后,形如下面一段代码,nif_init()函数是入口函数, */
+/* ErlNifEntry* nif_init(void); */
+/* ErlNifEntry* nif_init(void) { */
+/*   static ErlNifEntry entry = { 2, 3, */
+/*                                "hello_test", */
+/*                                sizeof(nif_funcs) / sizeof(*nif_funcs), */
+/*                                nif_funcs, */
+/*                                ((void *)0), */
+/*                                ((void *)0), */
+/*                                ((void *)0), */
+/*                                ((void *)0), */
+/*                                "beam.vanilla" }; */
+/*   ; */
+/*   return &entry; } */
